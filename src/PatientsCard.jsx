b0e939f49c9c5cff30d21606a97675ebe295ae61
fedi-nabli <fedinabli@gmail.com>
2023-12-08 22:47:43 +0100

    Fixed error

--- src/PatientsCard.jsx
+++ src/PatientsCard.jsx
@@ -5,7 +5,7 @@ import'./css.css'
 function PatientsCard({Patient , index}) {
   return (
     <div className='card'>
-        <NavLink to={'/Patients'+index}>
+        <NavLink to={'/Patients/'+index}>
       <h3 className='name'>{Patient.FullName}</h3>
       <ul className='informations'>
         <li>Age: {Patient.Age}</li>
